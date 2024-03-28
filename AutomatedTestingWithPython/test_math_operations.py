efef128bf0c5a1c3df2b8521687fdb92c213ef97
Sangeetha Nandakumar <98574336+sangeethankumar@users.noreply.github.com>
2024-03-28 14:05:07 +0100

    updated files

--- AutomatedTestingWithPython/test_math_operations.py
+++ AutomatedTestingWithPython/test_math_operations.py
@@ -1,13 +1,10 @@
-import pytest
 from math_operations import add_two_nums
 
-@pytest.mark.parametrize(
-        "num1, num2, result",
-        [
-            (1, 1, 2),
-            (10, 20, 30),
-            (2, -3, -1)
-        ]
-)
-def test_add_two_nums(num1, num2, result):
-    assert add_two_nums(num1, num2) == result
+def test_one_plus_one_is_two():
+    assert add_two_nums(1, 1) == 2
+
+def test_one_minus_one_is_zero():
+    assert add_two_nums(1, -1) == 0
+
+def test_one_pt_five_plus_ten_is_eleven_pt_five():
+    assert add_two_nums(1, -1) == 0
